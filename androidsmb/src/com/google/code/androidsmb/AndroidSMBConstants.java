78565869b772f4276d8c39e24a160c343464cbfd
patrick.winters@gmail.com <patrick.winters@gmail.com@088a337a-fd4b-11de-93ae-8533dffd2106>
2010-01-09 19:12:55 +0000

    Initial Import

---
+++ androidsmb/src/com/google/code/androidsmb/AndroidSMBConstants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.google.code.androidsmb;
+
+/**
+ * Interface defining application constants
+ * 
+ * @author Patrick
+ *
+ */
+public interface AndroidSMBConstants {
+    // Debugging
+    public static final String TAG = "AndroidSMB";
+    public static final boolean DEBUG = true;
+    
+}
