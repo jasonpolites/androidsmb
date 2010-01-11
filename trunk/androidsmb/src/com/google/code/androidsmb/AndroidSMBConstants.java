df1e7afd09cae89d3ceb6c82695bc760aa895887
patrick.winters@gmail.com <patrick.winters@gmail.com@088a337a-fd4b-11de-93ae-8533dffd2106>
2010-01-11 02:09:38 +0000

    Need to attach a logger to the SMB server to figure out what's happening with those network threads.

--- trunk/androidsmb/src/com/google/code/androidsmb/AndroidSMBConstants.java
+++ trunk/androidsmb/src/com/google/code/androidsmb/AndroidSMBConstants.java
@@ -15,5 +15,6 @@ public interface AndroidSMBConstants {
     public static final boolean DEBUG = true;
     public static final int RUNNING = 1;
     public static final int STOPPED = 0;
+    public static final String LOGGER = "Logger";
     
 }
